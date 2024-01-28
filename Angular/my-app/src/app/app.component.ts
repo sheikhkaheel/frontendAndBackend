c61a58d617b23094c7b3079e5c7ad01363125f0e
sheikhkaheel <sheikhkaheel@gmail.com>
2024-01-28 19:41:11 +0530

    added dropdowncomponents

--- Angular/my-app/src/app/app.component.ts
+++ Angular/my-app/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DemoComponent } from './demo/demo.component';
+import { ParentComponent } from './Parent/parent.component';
+import { GeneralDropDownComponent } from './general-drop-down/general-drop-down.component';
+import { DistrictDropDownComponent } from './district-drop-down/district-drop-down.component';
+import { StudentsDropdownComponent } from './students-dropdown/students-dropdown.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DemoComponent],
+  imports: [RouterOutlet, ParentComponent,GeneralDropDownComponent,DistrictDropDownComponent,StudentsDropdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
